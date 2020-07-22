ae99f0a5483bcd2e6f059b5a83ea26cdd89b5da0
Prof-Rodrigo-Silva <profrodrigorosadasilva@gmail.com>
2020-07-22 00:30:31 -0300

    First Update

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Frases do Dia"),
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16),
+        width: double.infinity, /* usa todo o espaco disponivel*/
+        /*decoration: BoxDecoration(
+          border: Border.all(width: 3, color: Colors.amber)
+        ),*/
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("images/logo.png"),
+            Text(
+              "Clique abaixo para gerar uma frase!",
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                fontSize: 17,
+                fontStyle: FontStyle.italic,
+                color: Colors.black
+              ),
+            ),
+            RaisedButton(
+              child: Text(
+                "Nova Frase",
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white
+                ),
+              ),
+              color: Colors.green,
+              onPressed: (){},
+            )
+          ],
+        ),
+
+
+      ),
+    );
+  }
+}
+
+
